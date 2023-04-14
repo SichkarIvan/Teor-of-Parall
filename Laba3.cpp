2f72e46378a498978db55d79d94f056e5d198177
SichkarIvan <ivansickar5225@gmail.com>
2023-04-14 05:22:45 +0000

    add comments

--- Laba3.cpp
+++ Laba3.cpp
@@ -10,16 +10,16 @@
 
 using namespace std;
 
+/////////////////////////////////////////////////////////////////////
 // Значения по умолчанию
 double eps = 1E-6;
 int iter_max = 1E6;
 int size = 128;
 
 
-
 int main(int argc, char *argv[])
 {
-
+/////////////////////////////////////////////////////////////////////
 // Ввод данных с консоли
     for(int arg = 0; arg < argc; arg++){ 
         stringstream stream;
@@ -37,11 +37,13 @@ int main(int argc, char *argv[])
         }
     }
     cout << "Settings:\n\tEPS: " << eps << "\n\tMax iteration: " << iter_max << "\n\tSize: " << size << 'x' << size << "\n\n";
- 
+/////////////////////////////////////////////////////////////////////
+// Создание массивов
+
     double* F = new double[size*size];
     double* Fnew = new double[size*size];
 
-
+/////////////////////////////////////////////////////////////////////
 // Ввод данных в грани матрицы F
     for(int i = 0; i < size; i++) {
                 at(F, 0, i) = 10 / size * i + 10;
@@ -49,7 +51,7 @@ int main(int argc, char *argv[])
                 at(F, size-1, i) = 10 / size * i + 20;
                 at(F ,i, size-1) = 10 / size * i + 20;
             }
-
+/////////////////////////////////////////////////////////////////////
 
     double error = 1.0;
     int iteration = 0;
@@ -57,7 +59,7 @@ int main(int argc, char *argv[])
     cublasHandle_t handler;
     cublasCreate(&handler);
 
-
+/////////////////////////////////////////////////////////////////////
 // Копирование данных F с CPU на GPU, выделение памяти под Fnew
 #pragma acc data copyin(F[:size*size]) create(Fnew[:size*size])
 {
@@ -70,12 +72,12 @@ int main(int argc, char *argv[])
         at(Fnew, size-1, j) = at(F, size-1, j);
     }
 
-
+/////////////////////////////////////////////////////////////////////
 // Основной цикл
+/////////////////////////////////////////////////////////////////////
     while (error > eps && iteration < iter_max )
     {
-
-    
+/////////////////////////////////////////////////////////////////////
 // Изменение матрицы Fnew
 #pragma acc parallel loop async(0)
         for( int j = 1; j < size-1; j++) {
@@ -83,13 +85,11 @@ int main(int argc, char *argv[])
             for( int i = 1; i < size-1; i++ ) {
                 at(Fnew, j, i) = 0.25 * ( at(F, j, i+1) + at(F, j, i-1) + at(F, j-1, i) + at(F, j+1, i));
             }
-
         }
-
 #pragma acc wait(0)
+/////////////////////////////////////////////////////////////////////
 
-
-
+/////////////////////////////////////////////////////////////////////
 // Изменение матрицы F
 #pragma acc parallel loop async(1)
         for( int j = 1; j < size-1; j++) {
@@ -97,26 +97,25 @@ int main(int argc, char *argv[])
             for( int i = 1; i < size-1; i++ ) {
                 at(F, j, i) = 0.25 * ( at(Fnew, j, i+1) + at(Fnew, j, i-1) + at(Fnew, j-1, i) + at(Fnew, j+1, i));
             }
-
         }
-
 #pragma acc wait(1)
+/////////////////////////////////////////////////////////////////////
 
         iteration+=2;
 
-
+/////////////////////////////////////////////////////////////////////
 // Каждые 250 итераций проверяем ошибку
         if (iteration % 250 == 0){
             int idx = 0;
             double alpha = -1.0;
-
+/////////////////////////////////////////////////////////////////////
 // Ищем максимум из разницы
 #pragma acc host_data use_device(Fnew, F)
             {
                 cublasDaxpy(handler, size * size, &alpha, Fnew, 1, F, 1);
                 cublasIdamax(handler, size * size, F, 1, &idx);
             }
-
+/////////////////////////////////////////////////////////////////////
 // Возвращаем ошибку на host
 #pragma acc update host(F[idx - 1]) 
 			error = abs(F[idx - 1]);
@@ -125,10 +124,9 @@ int main(int argc, char *argv[])
             cublasDcopy(handler, size * size, Fnew, 1, F, 1);
         }
     }
-
 }
 
-
+/////////////////////////////////////////////////////////////////////
 // Выведение результатов и очистка памяти
     cublasDestroy(handler);
 
